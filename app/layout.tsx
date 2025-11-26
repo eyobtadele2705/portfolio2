0ebfe5812f61fef7322a055ae81086350a9331e4
eyobtadele2705 <eyobtadele2705@gmail.com>
2025-11-26 15:44:20 +0300

    portifolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'eyob-dev',
+  description: 'Created by Eyob Tadele',
+  generator: '',
   icons: {
     icon: '/icon.svg',
   },
